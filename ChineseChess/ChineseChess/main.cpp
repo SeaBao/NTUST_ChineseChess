95c1298c5517e0738acf28f7aaff0c10b1c93884
SeaBao <tony24862486@gmail.com>
2019-04-18 21:05:33 +0800

    Change color

--- ChineseChess/ChineseChess/main.cpp
+++ ChineseChess/ChineseChess/main.cpp
@@ -4,7 +4,7 @@
 int main() {
 	HANDLE hOut;
 	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hOut, FOREGROUND_GREEN);
+	SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_BLUE);
 
 	system("pause");
 	return 0;
