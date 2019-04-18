cc6703d6b0091949daf46517ee1b37a42e69c71b
p458732 <p458732@kimo.com>
2019-04-18 21:09:49 +0800

    test  my

--- ChineseChess/ChineseChess/main.cpp
+++ ChineseChess/ChineseChess/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <Windows.h>
-
+using namespace std;
 int main() {
 	HANDLE hOut;
 	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_BLUE);
-
+	cout << "test";
 	system("pause");
 	return 0;
 }
